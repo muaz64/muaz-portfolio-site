4eeaccbf146701ae356609d77d696f5daaa37aa8
muaz64 <muazmuhammad21@gmail.com>
2024-11-03 15:05:52 +0600

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 
 import './App.css'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  
   return (
     <div>
-      <Navbar></Navbar>
-     
+      <Navbar />
+      <Home />
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
