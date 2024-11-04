b142fa722fafe34346dc2e80de078bf71617832a
muaz64 <muazmuhammad21@gmail.com>
2024-11-05 00:35:01 +0600

    update 5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Home() {
   return (
-    <section id="home" className="bg-primary-content h-screen flex items-center justify-center text-center">
+    <section id="home" className="flex items-center justify-center text-center my-36">
       <div>
       <h2 className='text-teal text-3xl font-semibold'>Hi there! I'm Muaz Muhammad</h2>
         <h1 className="text-4xl md:text-6xl font-bold text-charcoal-gray">Welcome to My Portfolio</h1>
         <p className="text-lg md:text-xl mt-4 text-gray-700">
           I'm an aspiring engineer and front-end developer. I create beautiful, user-friendly websites.
         </p>
-        <a href="#services" className="mt-6 inline-block px-8 py-3 text-white bg-teal hover:bg-charcoal-gray rounded-md">
+        <a href="#services" className="mt-6 inline-block px-8 py-3 text-white bg-neutral-500 hover:bg-teal rounded-md">
           Learn More
         </a>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,81 @@
-import React from 'react';
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const menu = [
+    <>
+      <li className="button my-4 cursor-pointer lg:mx-3">
+        <a href="#home" className="text-error hover:text-teal px-4 py-1 block">
+          Home
+        </a>
+      </li>
+      <li className="button my-4 cursor-pointer lg:mx-3">
+        <a href="#about" className="text-error hover:text-teal px-4 py-1 block">
+          About
+        </a>
+      </li>
+      <li className="button my-4 cursor-pointer lg:mx-3">
+        <a href="#services" className="text-error hover:text-teal px-4 py-1 block">
+          Services
+        </a>
+      </li>
+      <li className="button my-4 cursor-pointer lg:mx-3">
+        <a href="#projects" className="text-error hover:text-teal px-4 py-1 block">
+          Projects
+        </a>
+      </li>
+      <li className="button my-4 cursor-pointer lg:mx-3">
+        <a href="#contact" className="text-error hover:text-teal px-4 py-1 block">
+          Contact
+        </a>
+      </li>
+    </>,
+  ];
   return (
-    <nav className="bg-charcoal-gray text-black fixed w-full shadow-md">
-      <div className="container mx-auto flex justify-between items-center py-4">
-        <h1 className="text-2xl font-bold">Muaz Muhammad</h1>
-        <div className='dropdown'>
-        <ul tabIndex={0} className="hidden md:flex space-x-6 font-semibold">
-          <li><a href="#home" className="hover:text-teal">Home</a></li>
-          <li><a href="#about" className="hover:text-teal">About</a></li>
-          <li><a href="#services" className="hover:text-teal">Services</a></li>
-          <li><a href="#projects" className="hover:text-teal">Projects</a></li>
-          <li><a href="#contact" className="hover:text-teal">Contact</a></li>
-        </ul>
+    <div className="sticky top-3 z-50">
+      <div className="mx-5">
+        <div className="navbar font-medium">
+           <div className="navbar-start"><h1 className="button p-3 text-2xl hover:text-teal font-bold">Muaz</h1></div>
+          <div className="navbar-end flex-grow lg:flex-grow-0">
+            <div className="dropdown">
+              <div className="parent-div p-2 block lg:hidden">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="child-div p-3 lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h7"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="parent-div dropdown-content rounded-box z-[1] mt-5 p-4"
+              >
+                {menu}
+              </ul>
+            </div>
+          </div>
+          <div className="navbar-end   hidden lg:flex flex-grow justyfy-end w-10/12">
+            <ul tabIndex={0} className="flex parent-div">
+              {menu}
+            </ul>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
