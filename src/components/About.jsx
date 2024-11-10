0175dfec9345d9d642ffbaeddee13879a7c4c699
muaz64 <muazmuhammad21@gmail.com>
2024-11-10 23:15:21 +0600

    about update 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import AboutImage from '../assets/muazpp.jpg'
+import AboutImage from '../assets/muazpp.jpg';
+import "./About.css";
 
 function About() {
   return (
-    <section id="about" className="py-16">
-      <h2 className="text-3xl font-bold text-center text-secondary mb-10">About Me</h2>
-      <div className="container mx-auto flex flex-col md:flex-row items-center">
-      
-        <img src={AboutImage} alt="Profile" className="w-50 h-50 rounded-full mb-6 md:mb-0 md:mr-10"/>
-        <div>
-          <p className="text-lg text-gray-700">
+    <section id="about" className="py-20">
+      <h2 className="text-3xl font-bold text-center text-secondary mb-20 mx-auto uppercase">About Me</h2>
+
+      <div className="container flex flex-col md:flex-row items-center parent-div mx-5 md:mx-10">
+        <img src={AboutImage} alt="Profile" className="w-50 h-50 rounded-full  lg:my-5 md:mb-0 md:mx-10"/>
+        <div className='mb-10 px-7 lg:pr-16 md:py-10'>
+          <p className="text-lg text-gray-700 text-justify">
           As an aspiring engineer, I am fueled by a passion for innovation and technology. I'm a front-end developer with a keen eye for detail and a passion for creating beautiful and functional websites. With a background in web development and a strong understanding of UI/UX design, I strive to deliver high-quality, user-centered experiences. I am proficient in HTML, CSS, JavaScript, and various front-end frameworks, and I'm always eager to learn and adapt to new technologies.
           </p>
-          <a href="/path/to/resume.pdf" download className="mt-4 inline-block px-6 py-2 text-white bg-teal hover:bg-charcoal-gray rounded-md">
+          <a href="/path/to/resume.pdf" download className="mt-4 inline-block px-6 py-2 text-white hover:text-secondary bg-teal-800 hover:bg-teal-400 rounded-md">
             Download Resume
           </a>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div>
       <h2 className=' text-2xl font-semibold'>Hi there! I'm <span className='text-teal-500'>Muaz Muhammad</span></h2>
         <h1 className="text-4xl md:text-6xl font-bold text-zinc-800 animate-slidein ...">Welcome to My Portfolio</h1>
-        <p className="text-lg md:text-xl mt-4 text-cyan-800 animate-slidein ...">
+        <p className="text-lg md:text-xl mt-4 font-bold text-cyan-800 animate-slidein ...">
           I'm an aspiring engineer and front-end developer. I create beautiful, user-friendly websites.
         </p>
         <a href="#services" className="mt-6 inline-block px-8 py-3 text-white bg-neutral-500 hover:bg-indigo-400 rounded-md">
